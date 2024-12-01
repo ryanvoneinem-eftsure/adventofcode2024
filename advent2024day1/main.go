790c455773899cebcc95081159a956237bbe69ed
Ryan von Einem <ryan.einem@eftsure.com.au>
2024-12-01 18:23:02 +1100

    day1 part1

--- advent2024day1/main.go
+++ advent2024day1/main.go
@@ -4,7 +4,27 @@ import (
     "os"
     "fmt"
     "bufio"
+
+    "strings"
+    "strconv"
+    "sort"
 )
+
+func collectLists(scanner *bufio.Scanner) (l, r []int) {
+    
+    l = []int{}
+    r = []int{}
+    for scanner.Scan() {
+        line := scanner.Text()
+        nums := strings.Split(line, "   ")
+        lN, _ := strconv.Atoi(nums[0])
+        rN, _ := strconv.Atoi(nums[1])
+        l = append(l, lN)
+        r = append(r, rN)
+    }
+
+    return
+}
 
 func main() {
 
@@ -13,20 +33,36 @@ func main() {
         filename = os.Args[1]
     }
 
-    scanner := createFileScanner(filename)
+    file, scanner := createFileScanner(filename)
+	defer file.Close()
 
+    l, r := collectLists(scanner)
+    // fmt.Printf("list l = %v\n", l)
+    // fmt.Printf("list r = %v\n", r)
+	sort.Ints(l)
+	sort.Ints(r)
+	
+	totalDiffs := 0
+	for i, v := range l {
+        diff := v - r[i]
+        if diff < 0 {
+            diff = -diff
+        }
+        totalDiffs += diff
+	}
+
+    fmt.Printf("answer: %v\n", totalDiffs)
 
 }
 
-func createFileScanner(filename string) scanner bufio.Scanner {
+func createFileScanner(filename string) (file *os.File, scanner *bufio.Scanner) {
     
     file, fileErr := os.Open(filename)
-    check(err)
-    defer file.Close()
+    check(fileErr)
 
     fmt.Println("file opened")
 
-    scanner := bufio.NewScanner(file)
+    scanner = bufio.NewScanner(file)
     return
 
 }
