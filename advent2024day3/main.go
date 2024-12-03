d73006a00366703b372248628d33e89976cf9856
Ryan von Einem <ryan.einem@eftsure.com.au>
2024-12-03 13:14:04 +1100

    day3 init

---
+++ advent2024day3/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "bufio"
+)
+
+func main() {
+
+    filename := "./input.txt"
+    if len(os.Args) > 1 {
+        filename = os.Args[1]
+    }
+
+    file, scanner := createFileScanner(filename)
+	defer file.Close()
+
+}
+
+func createFileScanner(filename string) (file *os.File, scanner *bufio.Scanner) {
+    
+    file, fileErr := os.Open(filename)
+    check(fileErr)
+
+    fmt.Println("file opened")
+
+    scanner = bufio.NewScanner(file)
+    return
+
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
