c9d90109ef839ff25653296a6bac332233e14be2
Ryan von Einem <ryan.einem@eftsure.com.au>
2024-12-08 00:35:18 +1100

    day5 part2

--- advent2024day5/main.go
+++ advent2024day5/main.go
@@ -10,13 +10,13 @@ import (
 )
 
 type PageRules struct {
-    Page string
-    SucceededBy []string
-    PreceededBy []string
+	Page        string
+	SucceededBy []string
+	PreceededBy []string
 }
 
 func (pr *PageRules) ToString() string {
-    return pr.Page + ": SucceededBy[" + strings.Join(pr.SucceededBy, ",") + "] PreceededBy[" + strings.Join(pr.PreceededBy, ",") + "]"
+	return pr.Page + ": SucceededBy[" + strings.Join(pr.SucceededBy, ",") + "] PreceededBy[" + strings.Join(pr.PreceededBy, ",") + "]"
 }
 
 func main() {
@@ -30,63 +30,142 @@ func main() {
 	defer file.Close()
 
 	rules := collectOrderingRules(scanner)
-    fmt.Println("Rules:")
-    for _, r := range rules {
-        fmt.Println(r.ToString())
-    }
+	fmt.Println("Rules:")
+	for _, r := range rules {
+		fmt.Println(r.ToString())
+	}
 
-    ordered := findOrderedUpdates(scanner, rules)
-    sumMedian := 0
-    fmt.Println("Ordered:")
-    for _, u := range ordered {
-        fmt.Println(u)
+	// Part 1
+	// updates := findOrderedUpdates(scanner, rules)
 
-        i := (len(u)/2)
-        m, _ := strconv.Atoi(u[i])
-        sumMedian += m
-    }
-    
-    fmt.Println("sum of median page:", sumMedian)
-    
+	// Part 2
+	updates := findUnorderedUpdates(scanner, rules)
+	updates = sortUpdates(updates, rules)
+
+	sumMedian := 0
+	for _, u := range updates {
+		fmt.Println(u)
+
+		i := (len(u) / 2)
+		m, _ := strconv.Atoi(u[i])
+		sumMedian += m
+	}
+
+	fmt.Println("sum of median page:", sumMedian)
+
+}
+
+func sortUpdates(updates [][]string, rules map[string]*PageRules) [][]string {
+
+	rulesUpdates := [][]*PageRules{}
+	for _, u := range updates {
+		rulesUpdate := []*PageRules{}
+		for _, p := range u {
+			rulesUpdate = append(rulesUpdate, rules[p])
+		}
+		rulesUpdates = append(rulesUpdates, rulesUpdate)
+	}
+	ordered := [][]string{}
+	for _, rulesUpdate := range rulesUpdates {
+		slices.SortFunc(rulesUpdate, func(a *PageRules, b *PageRules) int {
+			if len(a.PreceededBy) == 0 && len(b.PreceededBy) == 0 {
+				return 0
+			} else if len(a.PreceededBy) == 0 {
+				return -1
+			} else if len(b.PreceededBy) == 0 {
+				return 1
+			}
+
+			if slices.Contains(a.SucceededBy, b.Page) || slices.Contains(b.PreceededBy, a.Page) {
+				return -1
+			} else if slices.Contains(a.PreceededBy, b.Page) || slices.Contains(b.SucceededBy, a.Page) {
+				return 1
+			}
+			return 0
+		})
+		ou := []string{}
+		for _, ru := range rulesUpdate {
+			ou = append(ou, ru.Page)
+		}
+		ordered = append(ordered, ou)
+	}
+
+	return ordered
+
+}
+
+func findUnorderedUpdates(scanner *bufio.Scanner, rules map[string]*PageRules) [][]string {
+	ordered := [][]string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		update := strings.Split(line, ",")
+		applicable := []*PageRules{}
+		isOrdered := true
+		for _, p := range update {
+			applicable = append(applicable, rules[p])
+		}
+		for _, r := range applicable {
+			pIdx := slices.Index(update, r.Page)
+			for i := 0; i < pIdx; i++ {
+				if slices.Contains(r.SucceededBy, update[i]) {
+					isOrdered = false
+					break
+				}
+			}
+			for i := pIdx + 1; i < len(update); i++ {
+				if slices.Contains(r.PreceededBy, update[i]) {
+					isOrdered = false
+					break
+				}
+			}
+		}
+		if !isOrdered {
+			ordered = append(ordered, update)
+		}
+
+	}
+
+	return ordered
 }
 
 func findOrderedUpdates(scanner *bufio.Scanner, rules map[string]*PageRules) [][]string {
-    ordered := [][]string{}
+	ordered := [][]string{}
 
-    for scanner.Scan() {
-        line := scanner.Text()
-        update := strings.Split(line, ",")
-        applicable := []*PageRules{}
-        isOrdered := true
-        for _, p := range update {
-            applicable = append(applicable, rules[p])
-        }
-        for _, r := range applicable {
-            pIdx := slices.Index(update, r.Page)
-            for i := 0; i < pIdx; i++ {
-                if slices.Contains(r.SucceededBy, update[i]) {
-                    isOrdered = false
-                    break
-                }
-            }
-            for i := pIdx+1; i < len(update); i++ {
-                if slices.Contains(r.PreceededBy, update[i]) {
-                    isOrdered = false
-                    break
-                }
-            }
-        }
-        if isOrdered {
-            ordered = append(ordered, update)
-        }
+	for scanner.Scan() {
+		line := scanner.Text()
+		update := strings.Split(line, ",")
+		applicable := []*PageRules{}
+		isOrdered := true
+		for _, p := range update {
+			applicable = append(applicable, rules[p])
+		}
+		for _, r := range applicable {
+			pIdx := slices.Index(update, r.Page)
+			for i := 0; i < pIdx; i++ {
+				if slices.Contains(r.SucceededBy, update[i]) {
+					isOrdered = false
+					break
+				}
+			}
+			for i := pIdx + 1; i < len(update); i++ {
+				if slices.Contains(r.PreceededBy, update[i]) {
+					isOrdered = false
+					break
+				}
+			}
+		}
+		if isOrdered {
+			ordered = append(ordered, update)
+		}
 
-    }
+	}
 
-    return ordered
+	return ordered
 }
 
 func collectOrderingRules(scanner *bufio.Scanner) map[string]*PageRules {
-    rules := map[string]*PageRules{}
+	rules := map[string]*PageRules{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -95,22 +174,22 @@ func collectOrderingRules(scanner *bufio.Scanner) map[string]*PageRules {
 			return rules
 		}
 
-        pages := strings.Split(line, "|")
-        
-        ruleL := rules[pages[0]]
-        ruleR := rules[pages[1]]
+		pages := strings.Split(line, "|")
 
-        if ruleL == nil {
-            ruleL = &PageRules{Page: pages[0]}
-            rules[pages[0]] = ruleL
-        }
-        ruleL.SucceededBy = append(ruleL.SucceededBy, pages[1])
+		ruleL := rules[pages[0]]
+		ruleR := rules[pages[1]]
 
-        if ruleR == nil {
-            ruleR = &PageRules{Page: pages[1]}
-            rules[pages[1]] = ruleR
-        }
-        ruleR.PreceededBy = append(ruleR.PreceededBy, pages[0])
+		if ruleL == nil {
+			ruleL = &PageRules{Page: pages[0]}
+			rules[pages[0]] = ruleL
+		}
+		ruleL.SucceededBy = append(ruleL.SucceededBy, pages[1])
+
+		if ruleR == nil {
+			ruleR = &PageRules{Page: pages[1]}
+			rules[pages[1]] = ruleR
+		}
+		ruleR.PreceededBy = append(ruleR.PreceededBy, pages[0])
 
 	}
 
