2f498ac8d574a8e5e7c998be878158dabc3b02ae
Ryan von Einem <ryan.einem@eftsure.com.au>
2024-12-14 19:05:32 +1100

    day14 part2

--- advent2024day14/main.go
+++ advent2024day14/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -98,9 +99,16 @@ func main() {
     if len(os.Args) > 1 {
         filename = os.Args[1]
     }
-    steps := 100
+
+    // Part 1
+    // steps := 100
+    // if len(os.Args) > 2 {
+    //     steps, _ = strconv.Atoi(os.Args[2])
+    // }
+    
+    maxSteps := 100
     if len(os.Args) > 2 {
-        steps, _ = strconv.Atoi(os.Args[2])
+        maxSteps, _ = strconv.Atoi(os.Args[2])
     }
 
     file, scanner := createFileScanner(filename)
@@ -112,25 +120,142 @@ func main() {
     }
 
     robots := loadRobots(scanner)
-    // fmt.Println(robots)
+    fmt.Println(len(robots))
 
-    var q1,q2,q3,q4 int
-    for i := 0; i < len(robots); i++ {
-        robots[i] = robots[i].Move(steps, bounds)
-        if robots[i].Pos.WithinBounds(bounds.GetQuadrant(1)) {
-            q1++
-        } else if robots[i].Pos.WithinBounds(bounds.GetQuadrant(2)) {
-            q2++
-        } else if robots[i].Pos.WithinBounds(bounds.GetQuadrant(3)) {
-            q3++
-        } else if robots[i].Pos.WithinBounds(bounds.GetQuadrant(4)) {
-            q4++
+    // Part 1
+    // var q1,q2,q3,q4 int
+    // for i := 0; i < len(robots); i++ {
+    //     robots[i] = robots[i].Move(steps, bounds)
+    //     if robots[i].Pos.WithinBounds(bounds.GetQuadrant(1)) {
+    //         q1++
+    //     } else if robots[i].Pos.WithinBounds(bounds.GetQuadrant(2)) {
+    //         q2++
+    //     } else if robots[i].Pos.WithinBounds(bounds.GetQuadrant(3)) {
+    //         q3++
+    //     } else if robots[i].Pos.WithinBounds(bounds.GetQuadrant(4)) {
+    //         q4++
+    //     }
+    // }
+    // fmt.Println("q1",q1,"q2",q2,"q3",q3,"q4",q4)
+    // fmt.Println("safety factor",q1*q2*q3*q4)
+
+    board := make([][]bool, bounds.BR.Y)
+    for i := 0; i < len(board); i++ {
+        board[i] = make([]bool, bounds.BR.X)
+    }
+
+    // Part 2
+    steps := 0
+    for i := 0; i < maxSteps; i++ {
+        for j := 0; j < len(robots); j++ {
+            robots[j] = robots[j].Move(1, bounds)
+            board[robots[j].Pos.Y][robots[j].Pos.X] = true
+        }
+        // fmt.Println("step",i+1)
+        // printBoard(board)
+        if findFrame(3, board) {
+            steps = i+1
+            printBoard(board)
+            break
+        }
+        board = resetBoard(board)
+    }
+
+    fmt.Println("steps",steps)
+
+}
+
+func resetBoard(board [][]bool) [][]bool {
+
+    for y := 0; y < len(board); y++ {
+        for x := 0; x < len(board[0]); x++ {
+            board[y][x] = false
+        }
+    }
+    return board
+}
+
+func printBoard(board [][]bool) {
+    for y := 0; y < len(board); y++ {
+        string := ""
+        for x := 0; x < len(board[0]); x++ {
+            if board[y][x] {
+                string += "#"
+            } else {
+                string += " "
+            }
+        }
+        fmt.Println(string)
+    }
+}
+
+func findFrame(heuristic int, board [][]bool) bool {
+
+    // fmt.Println(board)
+
+    frameCorners := [8]*Vec2{}
+    for y := 0; y < len(board); y++ {
+        count := 0;
+        var start *Vec2
+        for x := 0; x < len(board[0]); x++ {
+            if board[y][x] {
+                if count == 0 {
+                    start = &Vec2{x,y}
+                }
+                count++
+            } else {
+                if count >= heuristic {
+                    if frameCorners[0] == nil {
+                        frameCorners[0] = start
+                        frameCorners[1] = &Vec2{x-1,y}
+                    } else {
+                        frameCorners[2] = start
+                        frameCorners[3] = &Vec2{x-1,y}
+                    }
+                    // fmt.Println("framesides",frameCorners)
+                }
+                count = 0
+            }
         }
     }
 
-    // fmt.Println(robots)
-    fmt.Println("q1",q1,"q2",q2,"q3",q3,"q4",q4)
-    fmt.Println("safety factor",q1*q2*q3*q4)
+    for x := 0; x < len(board[0]); x++ {
+        count := 0;
+        var start *Vec2
+        for y := 0; y < len(board); y++ {
+            if board[y][x] {
+                if count == 0 {
+                    start = &Vec2{x,y}
+                }
+                count++
+            } else {
+                if count >= heuristic {
+                    if frameCorners[4] == nil {
+                        frameCorners[4] = start
+                        frameCorners[5] = &Vec2{x,y-1}
+                    } else {
+                        frameCorners[6] = start
+                        frameCorners[7] = &Vec2{x,y-1}
+                    }
+                    // fmt.Println("framesides",frameCorners)
+                }
+                count = 0
+            }
+        }
+    }
+
+    return makesRect(frameCorners[0:8])
+
+}
+
+func makesRect(points []*Vec2) bool {
+    
+    // fmt.Println(points)
+    if slices.IndexFunc(points, func(p *Vec2)(bool){return p == nil}) != -1 {
+        return false
+    }
+    // fmt.Println(points)
+    return points[0].Equals(*points[4]) && points[1].Equals(*points[6]) && points[2].Equals(*points[5])
 
 }
 
